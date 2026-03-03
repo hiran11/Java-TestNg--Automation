a5d5696702cf4b9142e449a67ae433a54bf4928c
hiran11 <saranya.tirumalasetty@gmail.com>
2026-03-03 15:59:07 -0500

    Added config properties file , PropertiesUtil file

--- src/test/java/com/ui/pages/HomePage.java
+++ src/test/java/com/ui/pages/HomePage.java
@@ -3,21 +3,26 @@ package com.ui.pages;
 import org.openqa.selenium.By;
 
 import com.constants.Browser;
+import com.constants.Env;
 import com.utility.Browserutility;
+import static com.utility.PropertiesUtil.*;
 
 public class HomePage extends Browserutility {
 
-    private static final By LOGIN_LOCATOR = By.id("login-button");
     private static final By USERNAME_LOCATOR = By.id("user-name");
     private static final By PASSWORD_LOCATOR = By.id("password");
+    private static final By LOGIN_LOCATOR = By.id("login-button");
 
     public HomePage(Browser browserName) {
         super(browserName);
-        goToWebsite("https://www.saucedemo.com/");
+       // goToWebsite("https://www.saucedemo.com/");
+       // goToWebsite(PropertiesUtil.readProperty(Env.QA,"URL"));
+        goToWebsite(readProperty(Env.QA,"URL"));
     }
 
 
     public MyAccountPage doLoginwith(String userName, String password) {
+    	
         enterText(USERNAME_LOCATOR, userName);
         enterText(PASSWORD_LOCATOR, password);
         clickOn(LOGIN_LOCATOR);
--- src/test/java/com/utility/PropertiesUtil.java
+++ src/test/java/com/utility/PropertiesUtil.java
@@ -6,20 +6,33 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
+import com.constants.Env;
+
 public class PropertiesUtil {
 
-	public static void main(String[] args) throws IOException {
+	public static String readProperty(Env env,String propertyName) {
 		
 		System.out.println(System.getProperty("user.dir"));
-		File proFile = new File(System.getProperty("user.dir") +("\\config\\QA.properties") );
+		File proFile = new File(System.getProperty("user.dir") +("\\config\\"+env+".properties") );// QA
 	
-			FileReader fileReader = new FileReader(proFile);
-			
+			FileReader fileReader = null;
 			Properties properties = new Properties();
-			  properties.load(fileReader);
-			  String urlValue = properties.getProperty("URL");
-			   System.out.println(urlValue);
 			
+			try {
+				fileReader = new FileReader(proFile);
+				properties.load(fileReader);
+			} 
+			
+			catch (FileNotFoundException e) {
+				  e.printStackTrace();
+			}
+	     
+			catch (IOException e) {
+				  e.printStackTrace();
+			}
+			  
+			  String urlValue = properties.getProperty(propertyName.toUpperCase());// URL
+			   return(urlValue);
 
 }
 }
